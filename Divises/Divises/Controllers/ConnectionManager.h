21e25b95d8a68b5ecb238e8b4524620e5f76894f
Xavier Ramos Oliver <Xavi@MacBook-Pro-de-Xavier.local>
2016-05-29 16:40:24 +0200

    Folder distribution changed to fit MVC design pattern

---
+++ Divises/Divises/Controllers/ConnectionManager.h
@@ -0,0 +1,13 @@
+//
+//  ConnectionManager.h
+//  Divises
+//
+//  Created by Xavier Ramos Oliver on 29/5/16.
+//  Copyright © 2016 Xavier Ramos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ConnectionManager : NSObject
+
+@end
---
+++ Divises/Divises/Controllers/ConnectionManager.m
@@ -0,0 +1,13 @@
+//
+//  ConnectionManager.m
+//  Divises
+//
+//  Created by Xavier Ramos Oliver on 29/5/16.
+//  Copyright © 2016 Xavier Ramos. All rights reserved.
+//
+
+#import "ConnectionManager.h"
+
+@implementation ConnectionManager
+
+@end
